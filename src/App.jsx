0ae836101506142c02a6330898af6c9fd8fd8078
mateusleitao <mateuselpidio0@gmail.com>
2026-07-24 09:45:24 -0300

    feat: add main page layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return(
+  return (
     <>
-      <h1>
-        hi
-      </h1>
+      <body>
+        <nav class="navbar">
+          <ul class="pagelist">
+            <li>
+              <a href="https://github.com/mateusleitao" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="lyrics.html">Lyrics</a>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <div id="card">
+            <h1>Hi, I am Mateus</h1>
+            <p>
+              First of all, I want to explain what is this page. This page is a
+              project of me that look into develop something which is entirely
+              autoral, I'm compromised with no using of tools that work for me,
+              kinda like any LLM or autocompletion stuff. All the code here is
+              entirely built for me, I will not use any framework or software
+              that is not on my knowledge, everything used here will be studied
+              before applied and I will only apply if I have a comprehension of
+              what I'm doing.
+            </p>
+            <p>
+              Every update on this page is being recorded on its respective
+              repository, based in: this link at commits section
+            </p>
+          </div>
+        </main>
+
+        <footer></footer>
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
